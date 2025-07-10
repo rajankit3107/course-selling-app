29c22415f2dc6988853a43d2d393569db77c0ef2
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-07-10 23:38:51 +0530

    completed signup and signin routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,18 +1,25 @@
 import { Router } from "express";
-import { userSchema } from "../validators/index.js";
+import { signupSchema } from "../validators/index.js";
+import { signinSchema } from "../validators/index.js";
 import { User } from "../models/index.js";
+import bcrypt from "bcryptjs";
 
 const userRouter = Router();
 
 userRouter.post("/signup", async (req, res) => {
   const { email, password, firstName, lastName } = req.body;
   //   console.log(req.body);
-  const result = userSchema.safeParse(req.body);
-  if (!result) res.status(400).json({ message: `fill your details correctly` });
+  const result = signupSchema.safeParse(req.body);
+  console.log(result);
+  if (!result.success)
+    res
+      .status(400)
+      .json({ success: false, message: `fill your details correctly` });
 
   //check for existing user
   const existingUser = await User.findOne({ email });
-  if (existingUser) res.status(400).json({ message: `User already exists` });
+  if (existingUser)
+    res.status(400).json({ success: false, message: `User already exists` });
 
   //if no exisiting user, create new user
   try {
@@ -23,16 +30,41 @@ userRouter.post("/signup", async (req, res) => {
       lastName,
     });
 
-    res.status(201).json({ message: `user signed up successfully` });
+    res
+      .status(201)
+      .json({ success: true, message: `user signed up successfully` });
   } catch (error) {
     console.log(error);
   }
 });
 
-userRouter.post("/signin", (req, res) => {
-  res.json({
-    message: "singed In",
-  });
+userRouter.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  const result = signinSchema.safeParse(req.body);
+  //   console.log(result);
+  if (!result.success)
+    res.status(400).json({
+      success: false,
+      message: `fill your details correctly`,
+    });
+  try {
+    const user = await User.findOne({ email });
+
+    const passwordIsMatched = await bcrypt.compare(password, user.password);
+    // console.log(passwordIsMatched);
+
+    if (!passwordIsMatched)
+      res.status(400).json({
+        success: false,
+        message: `wrong credentials`,
+      });
+    res.status(200).json({
+      success: true,
+      message: `login successful`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 userRouter.get("/purchases", (req, res) => {
--- validators/index.js
+++ validators/index.js
@@ -1,11 +1,19 @@
-import z from "zod";
+import z, { email } from "zod";
 
-export const userSchema = z.object({
+export const signupSchema = z.object({
   email: z.email(),
   password: z
-    .number()
+    .string()
     .min(4, { error: `password should be more than 4 characters` })
     .max(8, { error: `password should be less than 8 characters` }),
   firstName: z.string(),
   lastName: z.string(),
+});
+
+export const signinSchema = z.object({
+  email: z.email(),
+  password: z
+    .string()
+    .min(4, { error: `password should be more than 4 characters` })
+    .max(8, { error: `password should be less than 8 characters` }),
 });
